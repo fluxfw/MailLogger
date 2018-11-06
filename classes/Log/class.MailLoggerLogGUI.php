fd93c5c3cfbee297d7f7b12849916163f1487b11
Florian <fw@studer-raimann.ch>
2018-11-06 07:14:27 +0100

    Fix

--- classes/Log/class.MailLoggerLogGUI.php
+++ classes/Log/class.MailLoggerLogGUI.php
@@ -43,7 +43,7 @@ class MailLoggerLogGUI {
 
 		switch (strtolower($next_class)) {
 			default:
-				if (!self::ilias()->access()->hasLogAccess()) {
+				if (!self::access()->hasLogAccess()) {
 					ilUtil::sendInfo(self::plugin()->translate("no_access", self:: LANG_MODULE_LOG), true);
 					self::dic()->ctrl()->redirectByClass(ilRepositoryGUI::class);
 				}
