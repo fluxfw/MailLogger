ad916675f8012e419a0e75ad2d89c8c1600a1900
Florian Wyss <fw@studer-raimann.ch>
2020-01-10 13:27:42 +0100

    Refactor

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -187,9 +187,7 @@ class Log extends ActiveRecord
 
 
     /**
-     * @param string $field_name
-     *
-     * @return mixed|null
+     * @inheritDoc
      */
     public function sleep(/*string*/ $field_name)
     {
@@ -209,10 +207,7 @@ class Log extends ActiveRecord
 
 
     /**
-     * @param string $field_name
-     * @param mixed  $field_value
-     *
-     * @return mixed|null
+     * @inheritDoc
      */
     public function wakeUp(/*string*/ $field_name, $field_value)
     {
