65f63901348cb3b24ab2f31a51e3133d63726520
sragfw <fw@studer-raimann.ch>
2021-01-29 15:57:14 +0100

    Add require compose in ctrl entry classes

--- vendor/srag/custominputguis/src/MultiSelectSearchNewInputGUI/class.OrgUnitAjaxAutoCompleteCtrl.php
+++ vendor/srag/custominputguis/src/MultiSelectSearchNewInputGUI/class.OrgUnitAjaxAutoCompleteCtrl.php
@@ -2,6 +2,8 @@
 
 namespace srag\CustomInputGUIs\MailLogger\MultiSelectSearchNewInputGUI;
 
+require_once __DIR__ . "/../../../../autoload.php";
+
 use ilOrgUnitPathStorage;
 
 /**
--- vendor/srag/devtools/src/class.DevToolsCtrl.php
+++ vendor/srag/devtools/src/class.DevToolsCtrl.php
@@ -2,6 +2,8 @@
 
 namespace srag\DevTools\MailLogger;
 
+require_once __DIR__ . "/../../../autoload.php";
+
 use Closure;
 use ilAdministrationGUI;
 use ilDBConstants;
